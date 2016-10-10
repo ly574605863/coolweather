7c2731ca2b8ab3c7569d9b7b4eea4b00b64b07ee
unknown <>
2016-10-10 15:56:14 +0800

    新增数据库帮助类，各表对应的实体类

---
+++ app/src/main/java/com/example/ly574/coolweather/model/Province.java
@@ -0,0 +1,35 @@
+package com.example.ly574.coolweather.model;
+
+/**
+ * Created by ly574 on 2016/10/10.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
